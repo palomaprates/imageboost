be07547be9d69ee0d6ce9d3c9c51a43894b7144d
palomaprates <paloma.prates108@gmail.com>
2025-09-11 11:21:20 +0100

    sidebar component

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,66 @@
+"use client";
+import * as React from "react";
+import {
+  AudioWaveform,
+  // Bot,
+  Command,
+  GalleryVerticalEnd,
+} from "lucide-react";
+
+import { NavProjects } from "@/components/nav-projects";
+import { NavUser } from "@/components/nav-user";
+import { TeamSwitcher } from "@/components/team-switcher";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
+    {
+      name: "Acme Inc",
+      logo: GalleryVerticalEnd,
+      plan: "Enterprise",
+    },
+    {
+      name: "Acme Corp.",
+      logo: AudioWaveform,
+      plan: "Startup",
+    },
+    {
+      name: "Evil Corp.",
+      logo: Command,
+      plan: "Free",
+    },
+  ],
+};
+
+interface AppSidebarProps
+  extends React.ComponentPropsWithoutRef<typeof Sidebar> {
+  history: { id: number; description: string; image: string }[];
+}
+
+export function AppSidebar({ history, ...props }: AppSidebarProps) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavProjects history={history} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
