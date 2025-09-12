5397181e7d27a3c4aa984de7de1df81a1bae8295
palomaprates <paloma.prates108@gmail.com>
2025-09-12 15:11:29 +0100

    default files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Page from "./components/Page";
+
+function App() {
+  return (
+    <>
+      <div className="w-screen h-screen">
+        <Page />
+      </div>
+    </>
+  );
+}
+
+export default App;
