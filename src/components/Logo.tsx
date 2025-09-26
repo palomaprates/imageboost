940a630c934124abbddf9536dd3a1d8eef9a0056
palomaprates <paloma.prates108@gmail.com>
2025-09-26 15:48:37 +0100

    create and add logo (test)

---
+++ src/components/Logo.tsx
@@ -0,0 +1,9 @@
+import { LuImageUp } from "react-icons/lu";
+
+export default function Logo() {
+  return (
+    <div className="font-extralight flex items-center h-10 gap-1 justify-center">
+      <LuImageUp className="text-gray-700" /> ImageBoost
+    </div>
+  );
+}
