7f1e9f15164b1675e5e9746f9087034d8cf62c67
palomaprates <paloma.prates108@gmail.com>
2025-09-26 15:47:53 +0100

    set images size

--- src/components/DisplayImages.tsx
+++ src/components/DisplayImages.tsx
@@ -7,21 +7,14 @@ export default function DisplayImages({
     <div className="flex items-center justify-center gap-10">
       {imageUrls && (
         <div className="mt-4 flex flex-col items-center justify-center">
-          <img
-            src={imageUrls[0]}
-            alt="Original"
-            className="mb-2"
-            width={250}
-            height={250}
-          />
           <div className="flex gap-4 items-center justify-center">
             {imageUrls.slice(1).map((url, i) => (
               <img
                 key={i}
                 src={url}
                 alt={`Variação ${i + 1}`}
-                width={250}
-                height={250}
+                width={400}
+                height={300}
               />
             ))}
           </div>
