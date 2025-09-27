cb4d28972a539095e36fb063f8c3c1fa7c0b919e
palomaprates <paloma.prates108@gmail.com>
2025-09-27 15:37:41 +0100

    update style

--- src/components/ImageEditor.tsx
+++ src/components/ImageEditor.tsx
@@ -64,65 +64,65 @@ export default function ImageEditor() {
   };
 
   return (
-    <div className="p-4 max-w-l mx-auto flex flex-col justify-center items-center">
-      <div>
-        <div
-          className={`flex w-140 h-70 flex-col items-center justify-center border-2 border-dashed rounded-sm p-10 transition 
+    <div className="w-full gap-4 flex flex-col justify-center items-center">
+      {/* <div> */}
+      <div
+        className={`flex w-full h-70 flex-col items-center justify-center border-2 border-dashed rounded-sm p-10 transition 
         ${dragActive ? "border-blue-500 bg-blue-50" : "border-gray-400 bg-white"}`}
-          onDragOver={(e) => {
-            e.preventDefault();
-            setDragActive(true);
-          }}
-          onDragLeave={(e) => {
-            e.preventDefault();
-            setDragActive(false);
-          }}
-          onDrop={(e) => {
-            e.preventDefault();
-            setDragActive(false);
-            if (e.dataTransfer.files && e.dataTransfer.files[0]) {
-              handleFile(e.dataTransfer.files[0]);
+        onDragOver={(e) => {
+          e.preventDefault();
+          setDragActive(true);
+        }}
+        onDragLeave={(e) => {
+          e.preventDefault();
+          setDragActive(false);
+        }}
+        onDrop={(e) => {
+          e.preventDefault();
+          setDragActive(false);
+          if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+            handleFile(e.dataTransfer.files[0]);
+          }
+        }}
+      >
+        <input
+          id="fileInput"
+          className="hidden"
+          type="file"
+          accept="image/*"
+          onChange={(e) => {
+            if (e.target.files && e.target.files[0]) {
+              handleFile(e.target.files[0]);
+              e.target.value = "";
             }
           }}
+        />
+        <label
+          htmlFor="fileInput"
+          className="flex items-center gap-2 bg-gray-500 hover:bg-gray-700 text-white font-light px-6 py-3 rounded-sm cursor-pointer transition"
         >
-          <input
-            id="fileInput"
-            className="hidden"
-            type="file"
-            accept="image/*"
-            onChange={(e) => {
-              if (e.target.files && e.target.files[0]) {
-                handleFile(e.target.files[0]);
-                e.target.value = "";
-              }
-            }}
-          />
-          <label
-            htmlFor="fileInput"
-            className="flex items-center gap-2 bg-gray-500 hover:bg-gray-700 text-white font-light px-6 py-3 rounded-sm cursor-pointer transition"
-          >
-            <LuImagePlus />
-            Selecione sua imagem
-          </label>
-          <p className="text-gray-900 mt-4 font-light">Ou arraste aqui</p>
-        </div>
-        {preview && (
-          <FileDetails
-            preview={preview}
-            fileName={image?.name ?? ""}
-            fileSize={image?.size ?? 0}
-            onRemove={handleRemove}
-          />
-        )}
-
-        <button
-          onClick={handleUpload}
-          className="mt-4 p-2 w-140 bg-gray-900 text-white rounded-sm cursor-pointer font-light"
-          disabled={loading || !image}
-        >
-          {loading ? "Carregando..." : "Enviar"}
-        </button>
+          <LuImagePlus />
+          Selecione sua imagem
+        </label>
+        <p className="text-gray-900 mt-4 font-light">Ou arraste aqui</p>
       </div>
+      {preview && (
+        <FileDetails
+          preview={preview}
+          fileName={image?.name ?? ""}
+          fileSize={image?.size ?? 0}
+          onRemove={handleRemove}
+        />
+      )}
+
+      <button
+        onClick={handleUpload}
+        className="p-2 w-full bg-gray-900 text-white rounded-sm cursor-pointer font-light"
+        disabled={loading || !image}
+      >
+        {loading ? "Carregando..." : "Enviar"}
+      </button>
+      {/* </div> */}
       {/* <DisplayImages imageUrls={imageUrls} /> */}
     </div>
   );
