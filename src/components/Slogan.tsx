fdc8ee352ca10f1e413ed7939f520a5215c25317
palomaprates <paloma.prates108@gmail.com>
2025-09-27 15:54:16 +0100

    update slogan

--- src/components/Slogan.tsx
+++ src/components/Slogan.tsx
@@ -1,12 +1,14 @@
 export default function Slogan() {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <span className="text-3xl text-black font-bold">
+    <div className="flex flex-col justify-center items-center mb-10">
+      <span className="text-2xl text-black font-bold">
         Seu cardápio mais apetitoso, sua marca mais forte
       </span>
-      <span className="text- text-shadow-gray-800 font-light">
-        Valorize sua identidade visual e conquiste clientes com fotos de comida
-        ainda mais atraentes.
+      <span className="text-l text-shadow-gray-800 font-light mt-3">
+        Valorize sua identidade visual e conquiste clientes
+      </span>
+      <span className="text-l text-shadow-gray-800 font-light">
+        com fotos de comida ainda mais atraentes.
       </span>
     </div>
   );
