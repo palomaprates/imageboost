5c7a3fda55ec22e0f7209b5ab02f68f983f55e1e
palomaprates <paloma.prates108@gmail.com>
2025-09-27 15:41:59 +0100

    add slogan on homepage (test)

---
+++ src/components/Slogan.tsx
@@ -0,0 +1,13 @@
+export default function Slogan() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <span className="text-3xl text-black font-bold">
+        Seu cardápio mais apetitoso, sua marca mais forte
+      </span>
+      <span className="text- text-shadow-gray-800 font-light">
+        Valorize sua identidade visual e conquiste clientes com fotos de comida
+        ainda mais atraentes.
+      </span>
+    </div>
+  );
+}
