e9fe9d4c1c72d6acfe34260c809565bece2674ad
palomaprates <paloma.prates108@gmail.com>
2025-09-30 17:43:06 +0100

    update logo style

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,8 +2,8 @@ import { LuImageUp } from "react-icons/lu";
 
 export default function Logo() {
   return (
-    <div className="font-bold flex items-center h-10 gap-1 justify-center">
-      <LuImageUp className="text-red-600 font-semibold font-montserrat" />{" "}
+    <div className="font-bold flex items-center h-10 gap-1 justify-center select-none">
+      <LuImageUp className="text-red-600 font-semibold" />
       ImageBoost
     </div>
   );
