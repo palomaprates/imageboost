c80dde943fadf60ef0f3af87fc3c70e60a65ca1d
palomaprates <paloma.prates108@gmail.com>
2025-09-30 17:44:00 +0100

    att

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -3,17 +3,19 @@ import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { Outlet } from "@tanstack/react-router";
 import { useIsMobile } from "@/hooks/use-mobile";
 import SidebarButton from "./SidebarButton";
+import Header from "./Header";
 
 export default function Page() {
   const isMobile = useIsMobile();
   return (
-    <div className="bg-transparent">
+    <div>
       <SidebarProvider>
         <AppSidebar />
         <SidebarInset className="bg-transparent">
+          <Header />
           {isMobile && (
             <header className="fixed flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-              <div className="cursor-pointer flex items-center gap-2 px-4">
+              <div className="cursor-pointer flex items-center justify-center pt-4 px-4">
                 <SidebarButton />
               </div>
             </header>
