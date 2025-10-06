4dade2e2d09371751f96aeeadcbe4e49ebddf253
palomaprates <paloma.prates108@gmail.com>
2025-10-06 18:54:29 +0100

    update slogan

--- src/components/Slogan.tsx
+++ src/components/Slogan.tsx
@@ -2,11 +2,12 @@ export default function Slogan() {
   return (
     <div className="w-full text-center">
       <h1 className="font-montserrat text-4xl  text-gray-900 mb-3">
-        Seu cardápio mais apetitoso, <br /> sua marca mais forte
+        Seu cardápio mais profissional
+        <br /> sua marca mais forte
       </h1>
       <p className="font-montserrat text-gray-600 mb-8">
-        Valorize sua identidade visual e conquiste clientes com fotos
-        <br /> de comida ainda mais atraentes.
+        Valorize sua identidade visual e conquiste mais clientes
+        <br /> deixando o seu cardápio ainda mais profissional.
       </p>
     </div>
   );
