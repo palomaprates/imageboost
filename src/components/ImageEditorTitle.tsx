b57dfac813de124155a255593ce24213e59e2d7e
palomaprates <paloma.prates108@gmail.com>
2025-10-06 18:54:17 +0100

    change imageEditor route

---
+++ src/components/ImageEditorTitle.tsx
@@ -0,0 +1,14 @@
+export default function ImageEditorTitle() {
+  return (
+    <div className="w-full text-center">
+      <h1 className="font-montserrat text-4xl text-gray-900 mb-3">
+        Melhore o seu cardápio
+        <br /> em apenas um clique
+      </h1>
+      <p className="font-montserrat text-gray-600 mb-8">
+        Ideal para melhorar a identidade visual do seu cardápio
+        <br /> e fortalecer a sua marca
+      </p>
+    </div>
+  );
+}
---
+++ src/routes/editImage.tsx
@@ -0,0 +1,18 @@
+import ImageEditor from "@/components/ImageEditor";
+import ImageEditorTitle from "@/components/ImageEditorTitle";
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/editImage")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return (
+    <div className="flex items-center justify-center w-full mx-auto my-auto px-7 sm:px-12 max-w-[700px]">
+      <div className="flex flex-col items-center justify-center w-full mx-auto my-auto">
+        <ImageEditorTitle />
+        <ImageEditor />
+      </div>
+    </div>
+  );
+}
