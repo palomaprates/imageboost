ab208287949852c6980b1737bebb0d24104749d2
palomaprates <paloma.prates108@gmail.com>
2025-10-06 18:55:06 +0100

    update homepage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,28 @@
-import ImageEditor from "@/components/ImageEditor";
+import ImageCompare from "@/components/ImageCompare";
 import Slogan from "@/components/Slogan";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate({ to: "/editImage" });
+  };
   return (
     <div className="flex items-center justify-center w-full mx-auto my-auto px-7 sm:px-12 max-w-[700px]">
       <div className="flex flex-col items-center justify-center w-full mx-auto my-auto">
         <Slogan />
-        <ImageEditor />
+        <ImageCompare />
+        <button
+          onClick={handleNavigate}
+          className="my-8 w-full bg-white hover:bg-gray-100 text-purple-800
+                     font-montserrat text-l rounded-xl px-4 sm:px-6 py-3 shadow-md transition cursor-pointer text-sm"
+        >
+          Editar Imagem
+        </button>
       </div>
     </div>
   );
