f1a63411c01cb377b7aeacf6c08d12c9d3ad79e6
palomaprates <paloma.prates108@gmail.com>
2025-10-12 19:22:30 +0100

    fix login function and redirect to edit page

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -14,6 +14,10 @@ function LoginPage() {
   async function handleGoogleLogin() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
+      options: {
+        redirectTo: `${window.location.origin}/auth/callback`,
+        queryParams: { prompt: "select_account" },
+      },
     });
     if (error) setError(error.message);
   }
