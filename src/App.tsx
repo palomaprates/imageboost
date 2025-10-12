dd9e3b011c99f5705bd49401ceff6bc14d0a8fd4
palomaprates <paloma.prates108@gmail.com>
2025-10-12 10:07:09 +0100

    auth provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { RouterProvider, Router } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { AuthProvider } from "./context/AuthContext";
 
 const router = new Router({ routeTree });
 declare module "@tanstack/react-router" {
@@ -16,10 +17,12 @@ function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <div className="w-screen min-h-full bg-gradient-to-r from-purple-100/40 via-purple-100 to-[#e8dbf6]/30">
-          <RouterProvider router={router} />
-        </div>
-        <ReactQueryDevtools initialIsOpen={false} />
+        <AuthProvider>
+          <div className="w-screen min-h-full bg-gradient-to-r from-purple-100/40 via-purple-100 to-[#e8dbf6]/30">
+            <RouterProvider router={router} />
+          </div>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </AuthProvider>
       </QueryClientProvider>
     </>
   );
---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,46 @@
+import { supabase } from "@/services/supabaseClient";
+import { createContext, useContext, useEffect, useState } from "react";
+
+type AuthContextType = {
+  user: any;
+  loading: boolean;
+};
+
+const AuthContext = createContext<AuthContextType>({
+  user: null,
+  loading: true,
+});
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const init = async () => {
+      const { data } = await supabase.auth.getSession();
+      setUser(data?.session?.user ?? null);
+      setLoading(false);
+    };
+    init();
+
+    const { data: subscription } = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        setUser(session?.user ?? null);
+        setLoading(false);
+      }
+    );
+
+    return () => subscription.subscription.unsubscribe();
+  }, []);
+
+  console.log("user", user);
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
