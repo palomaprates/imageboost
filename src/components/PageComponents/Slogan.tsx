18b9d10cbc80c4b249e4d9cc321e70ec520a4698
palomaprates <paloma.prates108@gmail.com>
2025-10-19 09:59:30 +0100

    change slogan

--- src/components/PageComponents/Slogan.tsx
+++ src/components/PageComponents/Slogan.tsx
@@ -2,8 +2,8 @@ export default function Slogan() {
   return (
     <div className="w-full text-center select-none">
       <h1 className="font-montserrat text-4xl  text-gray-900 mb-3">
-        Seu cardápio mais profissional
-        <br /> sua marca mais forte
+        Otimize suas fotos
+        <br /> com ImageBoost
       </h1>
       <p className="font-montserrat text-gray-600 mb-8">
         Valorize sua identidade visual e conquiste mais clientes
