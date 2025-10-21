ba727a25a7773efd11d8ea8f1967529c4159598b
palomaprates <paloma.prates108@gmail.com>
2025-10-21 13:41:29 +0100

    update image at home page

--- src/components/ImageComponents/ImageCompare.tsx
+++ src/components/ImageComponents/ImageCompare.tsx
@@ -7,13 +7,13 @@ import OriginalImage from "../../assets/OriginalImage.jpeg";
 
 export default function ImageCompare() {
   return (
-    <div className="max-w-xl rounded-2xl overflow-hidden">
+    <div className="w-full h-full rounded-2xl overflow-hidden">
       <ReactCompareSlider
         itemOne={<ReactCompareSliderImage src={OriginalImage} alt="Original" />}
         itemTwo={
           <ReactCompareSliderImage src={GeneratedImage} alt="Generated" />
         }
-        style={{ width: "100%", height: "100%" }}
+        style={{ width: "800px", height: "700px" }}
       />
     </div>
   );
