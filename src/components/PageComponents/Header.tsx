6eb93a0ae54fac5670f560dd5a1ed0694a261b4e
palomaprates <paloma.prates108@gmail.com>
2025-10-23 14:14:52 +0100

    fix: link in the logo instead header

--- src/components/PageComponents/Header.tsx
+++ src/components/PageComponents/Header.tsx
@@ -1,10 +1,9 @@
-import { Link } from "@tanstack/react-router";
 import Logo from "./Logo";
 
 export default function Header() {
   return (
-    <Link className="flex justify-center items-center h-20 w-full" to="/">
+    <div className="flex justify-center items-center h-20 w-full">
       <Logo />
-    </Link>
+    </div>
   );
 }
--- src/components/PageComponents/Logo.tsx
+++ src/components/PageComponents/Logo.tsx
@@ -1,10 +1,14 @@
+import { Link } from "@tanstack/react-router";
 import { LuImageUp } from "react-icons/lu";
 
 export default function Logo() {
   return (
-    <div className="font-bold flex items-center h-10 gap-1 justify-center select-none text-lg hover:text-xl transition duration-300">
+    <Link
+      className="font-bold flex items-center h-10 gap-1 justify-center select-none text-lg hover:text-xl transition duration-300"
+      to="/"
+    >
       <LuImageUp className="text-purple-800 font-semibold" />
       ImageBoost
-    </div>
+    </Link>
   );
 }
