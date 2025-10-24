0a29c105dda77e90a35771a0ba0c77c1805ae6ea
palomaprates <paloma.prates108@gmail.com>
2025-10-24 15:36:59 +0100

    style: change font size

--- src/components/PageComponents/Slogan.tsx
+++ src/components/PageComponents/Slogan.tsx
@@ -1,10 +1,10 @@
 export default function Slogan() {
   return (
-    <div className="md:w-[650px] text-center select-none">
-      <h1 className="font-montserrat md:text-5xl text-4xl text-gray-900 mb-3">
+    <div className="md:w-[500px] text-center select-none">
+      <h1 className="font-montserrat text-4xl text-gray-900 mb-3">
         Otimize suas fotos com ImageBoost
       </h1>
-      <p className="font-montserrat md:text-lg text-gray-600 mb-8">
+      <p className="font-montserrat text-gray-600 mb-8">
         Valorize sua identidade visual e conquiste mais clientes deixando o seu
         cardápio ainda mais profissional.
       </p>
