84c0514a42a5b436f0f06e6208a1de8d1c97d7bd
palomaprates <paloma.prates108@gmail.com>
2025-10-24 15:37:26 +0100

    att

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -324,7 +324,7 @@ function SidebarFooter({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="sidebar-footer"
       data-sidebar="footer"
-      className={cn("flex flex-col gap-2 p-2", className)}
+      className={cn("flex flex-col gap-2 p-2 ", className)}
       {...props}
     />
   );
@@ -353,7 +353,7 @@ function SidebarContent({ className, ...props }: React.ComponentProps<"div">) {
       data-slot="sidebar-content"
       data-sidebar="content"
       className={cn(
-        "flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden",
+        "flex min-h-0 flex-1 flex-col gap-2 overflow-auto  group-data-[collapsible=icon]:overflow-hidden",
         className
       )}
       {...props}
